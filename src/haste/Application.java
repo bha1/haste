026dca83f2cd9ec6fec796e99d6b024b77cba734
bha1 <peeyushbhawan@gmail.com>
2017-10-21 00:21:02 +1100

    sheet builder and constant file added

--- src/haste/Application.java
+++ src/haste/Application.java
@@ -7,6 +7,8 @@ public static void main(String[] args) {
 	//Console console =  System.console();
 	DataBaseConnector.connect(null);
 	new BannerPrinter().printBanner();
+	ExcelSheetGenerator generator = new ExcelSheetGenerator();
+	generator.sheetBuilder();
 	//console.printf("Haste Application Starting !!!!");
 	//console.printf("Haste Application Starting !!!!");
 }
---
+++ src/resources/HasteConstants.java
@@ -0,0 +1,12 @@
+package resources;
+
+public class HasteConstants {
+	public static final String EXCELFILENAME = "haste.xlsx";
+	
+	public static final String SHEET1 = "DCO Credit Card";
+	
+	public static final String SHEET2 = "DOCTA sheet";
+	
+	public static final String SHEET3 = "OBDX sheet";
+	
+}
