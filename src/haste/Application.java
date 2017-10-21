b69981bd04c8907e6415d3e8bf1abd5f514babd8
bha1 <peeyushbhawan@gmail.com>
2017-10-21 12:37:39 +1100

    poc complete

--- src/haste/Application.java
+++ src/haste/Application.java
@@ -2,9 +2,21 @@ package haste;
 
 import java.io.Console;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
 public class Application {
 public static void main(String[] args) {
 	//Console console =  System.console();
+	BasicConfigurator.configure();
+	Logger logger = Logger.getLogger(Application.class.getName());
+	Logger.getLogger(Application.class.getName()).setLevel(Level.INFO);
+	logger.debug("this is in debug mode");
+	logger.info("info mode");
+	logger.error("this is not working");
+	logger.warn("warning");
+	logger.fatal("fatal error");
 	DataBaseConnector.connect(null);
 	new BannerPrinter().printBanner();
 	ExcelSheetGenerator generator = new ExcelSheetGenerator();
--- src/resources/HasteConstants.java
+++ src/resources/HasteConstants.java
@@ -3,10 +3,7 @@ package resources;
 public class HasteConstants {
 	public static final String EXCELFILENAME = "haste.xlsx";
 	
-	public static final String SHEET1 = "DCO Credit Card";
+	public static final String[] SHEETS = {"DCO Credit Card","DOCTA sheet","OBDX sheet"};
 	
-	public static final String SHEET2 = "DOCTA sheet";
-	
-	public static final String SHEET3 = "OBDX sheet";
 	
 }
